3a2eba2a5598ffa2731b55345a8eaef8ba912f39
Rika Hashii <rika.hashii@ctc-g.co.jp>
2023-09-19 16:00:08 +0900

    Fix: 調整

--- packages/NAGATO/NetworkUtils/__init__.py
+++ packages/NAGATO/NetworkUtils/__init__.py
@@ -1,13 +1,13 @@
-from .SNMP import SNMP
-from robot.api.deco import library
+# from .SNMP import SNMP
+# from robot.api.deco import library
 
-__all__ = ["NetworkUtils"]
+# __all__ = ["NetworkUtils"]
 
-@library(scope="SUITE", version="1.0.0")
-class NetworkUtils(SNMP):
-    """NetworkUtils is a Robot Framework library that enables various processes 
+# @library(scope="SUITE", version="1.0.0")
+# class NetworkUtils(SNMP):
+#     """NetworkUtils is a Robot Framework library that enables various processes 
     
-    from the terminal required for network test.
-    """
+#     from the terminal required for network test.
+#     """
 
-    pass
+#     pass
