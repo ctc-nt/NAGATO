1d4ad9f18162ec79f11abd024f2a422ea98633b1
Rika Hashii <rika.hashii@ctc-g.co.jp>
2023-09-19 11:54:08 +0900

    Add: ntc-tamplateの実装sampleを追加

--- packages/NAGATO/__init__.py
+++ packages/NAGATO/__init__.py
@@ -1,3 +1,25 @@
 from IxNetworkLibrary import IxNetworkLibrary
 from NetmikoLibrary import NetmikoLibrary
 from NetworkUtils import NetworkUtils
+import ntc_templates
+import os
+import shutil
+
+# netmikoのtextfsmを参照するパスを設定
+# os.environ["NET_TEXTFSM"] = "templates"
+
+print(f"パスは{os.getcwd()}です。")
+
+# get ntc_templates path
+templates_path = os.path.join(ntc_templates.__path__[0], "templates")
+
+# 内容をコピーしてこっちのtemplatesディレクトリにもってくる
+# .textfsmはそのままコピー
+for file, i in os.listdir(templates_path):
+    if ".textfsm" in file:
+        new_path = shutil.move(templates_path[i], "templates")
+    if file == "index":
+        with open(templates_path[i], mode='a') as remote_index:
+            with open("index", mode='a') as local_index:
+                # indexは末尾に追記する
+                remote_index.write(local_index)
