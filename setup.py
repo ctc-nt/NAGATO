a13fef497c8e0946a0ca31fa3d704c8f79223c91
Rika Hashii <rika.hashii@ctc-g.co.jp>
2023-09-19 13:54:29 +0900

    Fix: 調整

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ import shutil
 print(f"パスは{os.getcwd()}です。")
 setup()
 
-import ntc_templates
-# get ntc_templates path
-templates_path = os.path.join(ntc_templates.__path__[0], "templates")
+# import ntc_templates
+# # get ntc_templates path
+# templates_path = os.path.join(ntc_templates.__path__[0], "templates")
 
-# 内容をコピーしてこっちのtemplatesディレクトリにもってくる
-# .textfsmはそのままコピー
-for file, i in os.listdir(templates_path):
-    if ".textfsm" in file:
-        new_path = shutil.move(templates_path[i], "templates")
-    if file == "index":
-        with open(templates_path[i], mode='a') as remote_index:
-            with open("index", mode='a') as local_index:
-                # indexは末尾に追記する
-                remote_index.write(local_index)
+# # 内容をコピーしてこっちのtemplatesディレクトリにもってくる
+# # .textfsmはそのままコピー
+# for file, i in os.listdir(templates_path):
+#     if ".textfsm" in file:
+#         new_path = shutil.move(templates_path[i], "templates")
+#     if file == "index":
+#         with open(templates_path[i], mode='a') as remote_index:
+#             with open("index", mode='a') as local_index:
+#                 # indexは末尾に追記する
+#                 remote_index.write(local_index)
