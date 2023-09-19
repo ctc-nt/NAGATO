16df0399e5a76b14194c632d04586ec80be3ad69
Rika Hashii <rika.hashii@ctc-g.co.jp>
2023-09-19 15:26:53 +0900

    Fix:  調整

--- packages/NAGATO/__init__.py
+++ packages/NAGATO/__init__.py
@@ -1,11 +1,4 @@
-from .IxNetworkLibrary import IxNetworkLibrary
-from .NetmikoLibrary import NetmikoLibrary
-from .NetworkUtils import NetworkUtils
+from IxNetworkLibrary import IxNetworkLibrary
+from NetmikoLibrary import NetmikoLibrary
+from NetworkUtils import NetworkUtils
 from robot.api.deco import library
-import ntc_templates
-import os
-import shutil
-
-@library(scope="SUITE", version="1.0.0")
-class NAGATO(IxNetworkLibrary, NetmikoLibrary, NetworkUtils):
-    pass
