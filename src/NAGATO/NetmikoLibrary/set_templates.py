9e213170a1b94ada034845822f262f6ce28f4f08
Rika Hashii <rika.hashii@ctc-g.co.jp>
2023-09-25 17:22:23 +0900

    Fix: コメントを追加

--- src/NAGATO/NetmikoLibrary/set_templates.py
+++ src/NAGATO/NetmikoLibrary/set_templates.py
@@ -5,17 +5,19 @@ import NAGATO
 
 # copy ntc-templates and merge it with nagato's templates.
 
-ntc_templates_path = os.path.join(ntc_templates.__path__[0], "templates")
-nagato_templates_path = os.path.join(NAGATO.__path__[0], "templates")
-
-if os.name == "posix":
-    nagato_data_path = os.path.join(os.environ["HOME"], ".NAGATO")
-if os.name == "nt":
-    nagato_data_path = os.path.join(os.environ["LOCALAPPDATA"], "NAGATO")
-
-merged_template_path = os.path.join(nagato_data_path, "templates")
-
 if not os.getenv("NET_TEXTFSM"):
+
+    # get installed templates path
+    ntc_templates_path = os.path.join(ntc_templates.__path__[0], "templates")
+    nagato_templates_path = os.path.join(NAGATO.__path__[0], "templates")
+
+    # set destination templates path
+    if os.name == "posix":
+        nagato_data_path = os.path.join(os.environ["HOME"], ".NAGATO")
+    if os.name == "nt":
+        nagato_data_path = os.path.join(os.environ["LOCALAPPDATA"], "NAGATO")
+    merged_template_path = os.path.join(nagato_data_path, "templates")
+
     if not os.path.isdir(merged_template_path):
         # copy all ntc_templates
         shutil.copytree(ntc_templates_path, merged_template_path)
