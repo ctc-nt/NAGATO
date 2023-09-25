3faf9676973094bfca70cc62a730145cf27916b0
Rika Hashii <rika.hashii@ctc-g.co.jp>
2023-09-25 17:34:56 +0900

    Fix: 変数名修正

--- src/NAGATO/NetmikoLibrary/set_templates.py
+++ src/NAGATO/NetmikoLibrary/set_templates.py
@@ -13,26 +13,25 @@ if not os.getenv("NET_TEXTFSM"):
 
     # set destination templates path
     if os.name == "posix":
-        nagato_data_path = os.path.join(os.environ["HOME"], ".NAGATO")
+        dest_templates_path = os.path.join(os.environ["HOME"], ".NAGATO", "templates")
     if os.name == "nt":
-        nagato_data_path = os.path.join(os.environ["LOCALAPPDATA"], "NAGATO")
-    merged_template_path = os.path.join(nagato_data_path, "templates")
+        dest_templates_path = os.path.join(os.environ["LOCALAPPDATA"], "NAGATO", "templates")
 
-    if not os.path.isdir(merged_template_path):
+    if not os.path.isdir(dest_templates_path):
         # copy all ntc_templates
-        shutil.copytree(ntc_templates_path, merged_template_path)
+        shutil.copytree(ntc_templates_path, dest_templates_path)
 
         # merge nagato_templates
         for file in os.listdir(nagato_templates_path):
             if ".textfsm" in file:
-                shutil.copy(os.path.join(nagato_templates_path, file), merged_template_path)
+                shutil.copy(os.path.join(nagato_templates_path, file), dest_templates_path)
 
             # merge index of nagato
             if file == "index":
-                with open(os.path.join(merged_template_path, "index"), mode='a') as ntc_index:
+                with open(os.path.join(dest_templates_path, "index"), mode='a') as ntc_index:
                     with open(os.path.join(nagato_templates_path, "index")) as nagato_index:
                         nagato_index_contents = nagato_index.read()
                         ntc_index.write(f"\n{nagato_index_contents}")
 
     # set NET_TEXTFSM
-    os.environ["NET_TEXTFSM"] = merged_template_path
+    os.environ["NET_TEXTFSM"] = dest_templates_path
