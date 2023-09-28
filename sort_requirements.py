6373206b86f79bce8a2364a8c98d006fbde6ce3a
Rika Hashii <rika.hashii@ctc-g.co.jp>
2023-09-28 16:26:51 +0900

    NTDEV-23440 Add: Actions CIを追加

---
+++ sort_requirements.py
@@ -0,0 +1,11 @@
+# sort of requirements file
+import sys
+
+for file in sys.argv[1:]:
+    with open(file) as f:
+        data = f.read().splitlines()
+        data.sort()
+        sorted_data = '\n'.join(data)
+
+    with open(file, mode="w") as f:
+        f.write(sorted_data)
