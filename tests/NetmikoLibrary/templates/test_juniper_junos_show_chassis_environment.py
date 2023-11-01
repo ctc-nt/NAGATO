c7716a4e71aaeec99a7b0b877d6e73eb924be2f9
Saori Yui <saori.yui@ctc-g.co.jp>
2023-11-01 09:00:27 +0000

    Formatter applied

--- tests/NetmikoLibrary/templates/test_juniper_junos_show_chassis_environment.py
+++ tests/NetmikoLibrary/templates/test_juniper_junos_show_chassis_environment.py
@@ -1,10 +1,11 @@
 import os
+
 import pytest
-from textfsm import TextFSM
 
 # cli_outputs.pyからテストデータを取得
 # 将来, AMIOSのバージョンアップに伴ってCLIの出力が変わった場合に, 変更するファイルが1つだけで済むようにする
 from _cli_outputs import show_chassis_environment
+from textfsm import TextFSM
 
 # テスト対象テンプレートファイル
 template_path = os.path.join(os.getcwd(), "src/NAGATO/templates/juniper_junos_show_chassis_environment.textfsm")
@@ -21,4 +22,4 @@ def test_juniper_junos_show_chassis_environment():
     output = re_table.ParseText(show_chassis_environment)
     print(f"\n{output=}")
 
-    assert output == [['PEM 0 ', 'OK'], ['PEM 1 ', 'OK'], ['PEM 2 ', 'Check'], ['PEM 3 ', 'Check'], ['Routing Engine 0 ', 'OK']]
+    assert output == [["PEM 0 ", "OK"], ["PEM 1 ", "OK"], ["PEM 2 ", "Check"], ["PEM 3 ", "Check"], ["Routing Engine 0 ", "OK"]]
