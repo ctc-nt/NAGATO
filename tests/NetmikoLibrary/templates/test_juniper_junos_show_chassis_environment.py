3f790f837b567c992b3abd27a490dbc1b8c71253
Saori Yui <saori.yui@ctc-g.co.jp>
2023-11-01 17:58:18 +0900

    NTDEV-26155 Add: 自作Template追加
    juniper_junos_show_chassis_enviornment.textfsmとそのUTを追加

---
+++ tests/NetmikoLibrary/templates/test_juniper_junos_show_chassis_environment.py
@@ -0,0 +1,24 @@
+import os
+import pytest
+from textfsm import TextFSM
+
+# cli_outputs.pyからテストデータを取得
+# 将来, AMIOSのバージョンアップに伴ってCLIの出力が変わった場合に, 変更するファイルが1つだけで済むようにする
+from _cli_outputs import show_chassis_environment
+
+# テスト対象テンプレートファイル
+template_path = os.path.join(os.getcwd(), "src/NAGATO/templates/juniper_junos_show_chassis_environment.textfsm")
+
+# テンプレートファイルを読み込む
+with open(template_path, mode="r") as f:
+    re_table = TextFSM(f)
+
+
+@pytest.mark.textfsm
+def test_juniper_junos_show_chassis_environment():
+    """Parse可能なこと"""
+
+    output = re_table.ParseText(show_chassis_environment)
+    print(f"\n{output=}")
+
+    assert output == [['PEM 0 ', 'OK'], ['PEM 1 ', 'OK'], ['PEM 2 ', 'Check'], ['PEM 3 ', 'Check'], ['Routing Engine 0 ', 'OK']]
