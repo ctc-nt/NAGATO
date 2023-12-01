182169a6ba605b9f48034cd7cea22c98b0da682a
Sayaka Baba <sayaka.baba@ctc-g.co.jp>
2023-12-01 01:38:21 +0000

    Formatter applied

--- Unittest/TextFSM/test_show_bundle_bundle-ether.py
+++ Unittest/TextFSM/test_show_bundle_bundle-ether.py
@@ -1,10 +1,11 @@
 import os
+
 import pytest
-from textfsm import TextFSM
 
 # cli_outputs.pyからテストデータを取得
 # 将来, AMIOSのバージョンアップに伴ってCLIの出力が変わった場合に, 変更するファイルが1つだけで済むようにする
 from _cli_outputs import show_bundle_bundle_ether
+from textfsm import TextFSM
 
 # テスト対象テンプレートファイル
 template_path = os.path.join(os.getcwd(), "Templates", "cisco_xr_show_bundle_bundle-ether.textfsm")
@@ -21,4 +22,4 @@ def test_show_ip():
     output = re_table.ParseText(show_bundle_bundle_ether)
     print(f"\n{output=}")
 
-    assert output == [[{'interface': 'Bundle-Ether10', 'status': 'Up', 'links': '1', 'port': ['Te0/0/0/38', 'Hu0/1/0/42'], 'device': 'Local', 'state': 'Active', 'port_id': '0x8000, 0x0000', 'b_w': '100000000'}]]
+    assert output == [[{"interface": "Bundle-Ether10", "status": "Up", "links": "1", "port": ["Te0/0/0/38", "Hu0/1/0/42"], "device": "Local", "state": "Active", "port_id": "0x8000, 0x0000", "b_w": "100000000"}]]
