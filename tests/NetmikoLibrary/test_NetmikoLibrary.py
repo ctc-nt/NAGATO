d6fb38bde683f2d337ab231bac53a24f5917fa08
Takumi Nohara <takumi.nohara.571@ctc-g.co.jp>
2023-12-01 20:59:04 +0900

    fix: glob argument compat

--- tests/NetmikoLibrary/test_NetmikoLibrary.py
+++ tests/NetmikoLibrary/test_NetmikoLibrary.py
@@ -18,9 +18,9 @@ def test_set_templates(clear_environ, clear_nagato_appdata):
 
     NetmikoLibrary()
 
-    file_num_ntc = len(glob.glob("*.textfsm", root_dir=join(ntc_templates.__path__[0], "templates")))
-    file_num_nagato = len(glob.glob("*.textfsm", root_dir=join(NAGATO.__path__[0], "templates")))
-    file_num_app = len(glob.glob("*.textfsm", root_dir=join(appdata_path, "templates")))
+    file_num_ntc = len(glob.glob(join(ntc_templates.__path__[0], "templates", "*.textfsm")))
+    file_num_nagato = len(glob.glob(join(NAGATO.__path__[0], "templates", "*.textfsm")))
+    file_num_app = len(glob.glob(join(appdata_path, "templates", "*.textfsm")))
 
     assert os.path.exists(appdata_path)
     assert file_num_app == (file_num_ntc + file_num_nagato)
