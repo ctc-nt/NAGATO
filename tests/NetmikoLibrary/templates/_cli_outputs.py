ff983e265866beec4be271ac7e55ebd1f68d3a8a
Sayaka Baba <sayaka.baba@ctc-g.co.jp>
2023-12-04 13:52:31 +0900

    Add: indexとテストコード追加

--- tests/NetmikoLibrary/templates/_cli_outputs.py
+++ tests/NetmikoLibrary/templates/_cli_outputs.py
@@ -18,3 +18,51 @@ Routing Engine status:
 Routing Engine status:
   Slot 1:
     Current state                  Backup"""
+
+# show bundle bundle-ether
+
+show_bundle_bundle_ether = """\
+Bundle-Ether10
+  Status:                                    Up
+  Local links <active/standby/configured>:   2 / 0 / 2
+  Local bandwidth <effective/available>:     110000000 (110000000) kbps
+  MAC address (source):                      9088.555a.b8bb (Chassis pool)
+  Inter-chassis link:                        No
+  Minimum active links / bandwidth:          1 / 1 kbps
+  Maximum active links:                      64
+  Wait while timer:                          2000 ms
+  Load balancing:                            
+    Link order signaling:                    Not configured
+    Hash type:                               Default
+    Locality threshold:                      None
+  LACP:                                      Not operational
+    Flap suppression timer:                  Off
+    Cisco extensions:                        Disabled
+    Non-revertive:                           Disabled
+  mLACP:                                     Not configured
+  IPv4 BFD:                                  Not configured
+  IPv6 BFD:                                  Not configured
+  Port                  Device           State        Port ID         B/W, kbps
+  --------------------  ---------------  -----------  --------------  ----------
+  Te0/0/0/38            Local            Active       0x8000, 0x0000    10000000
+      Link is Active
+  Hu0/1/0/42            Local            Active       0x8000, 0x0000   100000000
+      Link is Active
+"""
+
+# show ethernet oam event-log
+
+show_ethernet_oam_event_log = """\
+Mon Dec  4 11:27:06.463 JST
+Local Action Taken:
+    N/A    - No action needed         EFD    - Interface brought down using EFD
+    None   - No action taken          Err.D  - Interface error-disabled        
+    Logged - System logged           
+
+TenGigE0/0/0/38
+================================================================================
+                                                                       Breaching
+Time                    Type                    Loc'n Action Threshold Value
+----------------------- ----------------------- ----- ------ --------- ---------
+Mon Dec 04 11:26:38 JST Dying gasp              Local Logged       N/A       N/A
+"""
---
+++ tests/NetmikoLibrary/templates/test_cisco_xr_show_ethernet_oam_event-log.py
@@ -0,0 +1,24 @@
+import os
+import pytest
+from textfsm import TextFSM
+
+# cli_outputs.pyからテストデータを取得
+# 将来, AMIOSのバージョンアップに伴ってCLIの出力が変わった場合に, 変更するファイルが1つだけで済むようにする
+from _cli_outputs import show_ethernet_oam_event_log
+
+# テスト対象テンプレートファイル
+template_path = os.path.join(os.getcwd(), "src/NAGATO/templates", "cisco_xr_show_ethernet_oam_event-log.textfsm")
+
+# テンプレートファイルを読み込む
+with open(template_path, mode="r") as f:
+    re_table = TextFSM(f)
+
+
+@pytest.mark.textfsm
+def test_show_ip():
+    """Parse可能なこと"""
+
+    output = re_table.ParseText(show_ethernet_oam_event_log)
+    print(f"\n{output=}")
+
+    assert output == [['TenGigE0/0/0/38','Mon Dec 04 11:26:38 JST', 'Dying gasp']]
