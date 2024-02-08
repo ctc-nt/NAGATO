e2639820d60fca7d4a68ad761ce9d273df2a3b31
Takumi Nohara <takumi.nohara.571@ctc-g.co.jp>
2024-02-08 18:29:40 +0900

    translate

--- src/NAGATO/PcapFileReader.py
+++ src/NAGATO/PcapFileReader.py
@@ -6,44 +6,41 @@ from robot.api.deco import keyword, library
 
 @library(scope="SUITE", version="1.0.0")
 class PcapFileReader:
-    """PcapFileReaderはpcap形式のファイルを読み込み、データを取得するRobot Frameworkライブラリです。
+    """PcapFileReader is a Robot Framework library for packet capture file verification.
 
-    このライブラリはpysharkパッケージを利用しています。
-
-    初めに`Read Pcap File`を使用し、pcap形式のファイルを読み込んでください。
+    This library uses pyshark package. Therefore it needs tshark installed on a machine.
     """
 
     def __init__(self):
         pass
 
     @keyword
-    def read_pcap_file(self, filepath: str, **kwargs) -> FileCapture:
-        """``filepath`` に指定した場所のpcapファイルを読み込みます。
+    def read_pcap_file(self, source: str, **kwargs) -> FileCapture:
+        """Read the given pcap file and parse into FileCapture structure.
 
         Example:
-        | ${capture} = | `Read Pcap File` | /path/to/pcap/file.pcap |
+        | ${capture} = | `Read Pcap File` | source=/path/to/pcap/file.pcap |
         """
 
-        return FileCapture(filepath, **kwargs)
+        return FileCapture(source, **kwargs)
 
     @keyword
     def get_packet_data(self, capture: FileCapture, number: int, *args: str) -> str:
-        """読み込んだpcapファイルから ``number`` に指定した場所のパケットの情報を抜き出し、その中からデータを取得します。
+        """Return the given ``number`` packet data in ``capture`` .
 
-        可変長引数には取得するデータを階層ごとに分けて指定します。データの指定はpysharkの指定方法に準拠しています。
-        例えばpysharkで ``${var}.ip.src`` と表現される場合、可変長引数には ``ip`` , ``src`` の順に値を与えます。
+        In the variable arguments, give the value to retrieve separately for each hierarchy.
+        The value format follows the pyshark expression.
+        For example, when expressed as ``packet.ip.src`` in pyshark, the variable arguments are given values in the order of ``ip`` and ``src``.
 
         Example:
         | ${ipaddr_src} = | `Get Packet Data` | ${1} | ip | src |
         | Should Be Equal | ${ipaddr_src} | 192.168.1.1 |
         """
 
-        # Packetオブジェクトを取得
         packet: Packet = capture[number - 1]
         logger.write(packet.show(), level="INFO")
 
-        # 可変長引数に与えられた文字列をdata_strに連結させる
-        # 例えばip, srcを与えられた場合, packet.ip.srcとなる
+        # concatenate values given to variable arguments to data_str
         data_str = "packet"
 
         if args:
@@ -51,11 +48,11 @@ class PcapFileReader:
                 data_str += f".{layer}"
 
         try:
-            # eval関数で変数data_strの文字列を変数として扱い、packetオブジェクトの属性をstrで取得する
+            # Treat the value of data_str as Packet object
             return eval(f"str({data_str})")
 
         except AttributeError:
-            logger.write(f"{data_str}は存在しません。", level="ERROR")
+            logger.write(f"{data_str} does not exist.", level="ERROR")
             raise
 
         except Exception:
@@ -63,59 +60,54 @@ class PcapFileReader:
 
     @keyword
     def count_total_packets(self, capture: FileCapture) -> int:
-        """``filepath`` に指定した場所のpcapファイルを読み込み、パケットの総数をintで返します
+        """Return amount of packet in ``capture`` .
 
         Example:
-        | `Count Total Packets` | /path/to/pcap/file.pcap |
+        | ${number} = | `Count Total Packets` | /path/to/pcap/file.pcap |
+        | Should Be Equal | ${number} | 10000 |
         """
-
-        if capture is None:
-            raise FileNotFoundError("pcapファイルが読み込まれていません")
 
         capture.load_packets()
 
         return len(capture)
 
     @keyword
-    def expected_packet_should_exist(self, capture: FileCapture, **packet_info):
-        """引数で渡された``key=value``を含むパケットが、pcapファイルに存在するか確認するキーワードです。
+    def packet_should_exist(self, capture: FileCapture, **packet_info):
+        """Fails if there is no packet in ``capture`` that matches the conditions given to ``packet_info`` .
 
-        データの指定はpysharkの指定方法に準拠していますが、本キーワードでは可変長引数のkeyには以下のように記述してください。
-        ex) ip.src=192.168.1.1, icmp.type=8
+        In the ``packet_info`` , give the any conditions to verify. Conditions are expressed as pyshark format.
+
+        ex.) ip.src=192.168.1.1, icmp.type=8
+
+        Note: Python code cannot give keyword including periods as keyword arguments. Use dictionary unpacking instead.
 
         Example:
         | `Expected Packet Should Exist` | ip.src=192.168.1.1 | ip.dst=172.16.1.1 |
         """
 
-        # pcapファイルの総パケット数を取得
         total_number = self.count_total_packets(capture)
 
         for number in range(total_number):
-            # Packetオブジェクトを取得
+
             packet: Packet = capture[number]
 
-            # 引数によって指定されたパケットの条件数と``end_sign_count``の数が一致した場合、想定パケットは存在する
-            # ここでは初期化を実施
             end_sign_count = 0
             for key, value in packet_info.items():
                 try:
-                    # 可変長引数に与えられた文字列をdata_strに連結させる
                     data_str = "packet"
                     if packet_info:
                         data_str += f".{key}"
-                    # eval関数で変数data_strの文字列を変数として扱い、packetオブジェクトの属性をstrで取得する
+
                     output = eval(f"str({data_str})")
 
                     if output == value:
                         end_sign_count += 1
 
                 except AttributeError:
-                    # 可変超引数に与えられた条件に1つでも一致しなかった場合、次のパケットの確認に移る
                     continue
 
             if end_sign_count == len(packet_info):
                 logger.write(f"packet number: {number+1}\n{packet}")
-                # 可変朝引数に与えられた条件に全て一致した場合、全ての処理を終了する
                 return
 
-        raise Exception("想定パケットがpcapファイルに存在しません")
+        raise AssertionError("There are no packets matching the given conditions.")
